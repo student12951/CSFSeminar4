df684a17d72343603905309cb098406d6f206799
SanjarDeveloper <developer.sanjar@gmail.com>
2022-10-06 10:28:30 +0500

    init: some commit1

---
+++ main.py
@@ -0,0 +1,22 @@
+def encode(message):
+    encoded_message = ""
+    i = 0
+    while (i <= len(message) - 1):
+        count = 1
+        char = message[1]
+        j = i
+        while (j < len(message) - 1):
+            if (message[j] == message[j + 1]):
+                count += 1
+temp = "something"
+index = len(temp) - 1
+for x in temp:
+    char = ""
+    count = 0
+    countOfNum = 0
+    while (count < index):
+        if (x[count] == x[count + 1]):
+            char = x[count]
+            countOfNum += 1
+
+print(countOfNum)
